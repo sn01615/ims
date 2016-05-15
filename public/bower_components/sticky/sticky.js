511e3aa528707857c6d6b5e34fa46a6c4d8c5034
杨龙 <long.yang@xytinc.com>
2016-05-15 23:13:05 +0800

    [misc]

--- public/bower_components/sticky/sticky.js
+++ public/bower_components/sticky/sticky.js
@@ -57,7 +57,7 @@
 			{
 			// Building and inserting sticky note
 			$('.sticky-queue').prepend('<div class="sticky border-' + position + '" id="' + uniqID + '"></div>');
-			$('#' + uniqID).append('<img src="close.png" class="sticky-close" rel="' + uniqID + '" title="Close" />');
+			$('#' + uniqID).append('<img src="public/bower_components/sticky/close.png" class="sticky-close" rel="' + uniqID + '" title="Close" />');
 			$('#' + uniqID).append('<div class="sticky-note" rel="' + uniqID + '">' + note + '</div>');
 			
 			// Smoother animation
