74a5cc53de8540610e631626af0ae19e20092c7c
杨龙 <long.yang@xytinc.com>
2016-05-21 17:27:53 +0800

    [bugfix]大XML判断问题修复！

--- protected/extensions/XMLTool.php
+++ protected/extensions/XMLTool.php
@@ -21,12 +21,21 @@ class XMLTool
         $str = imsTool::removeNonPrintable($str, 'IsXML');
         
         $xml_parser = xml_parser_create();
-        if (! xml_parse($xml_parser, $str, true)) {
-            xml_parser_free($xml_parser);
-            return false;
-        } else {
-            return true;
+        $start = 0;
+        $blocksize = 1024 * 1024;
+        while (true) {
+            $_str = substr($str, $start, $blocksize);
+            $start += $blocksize;
+            if ($_str !== false) {
+                xml_parse($xml_parser, $_str);
+            } else {
+                if (! xml_parse($xml_parser, $_str, true)) {
+                    xml_parser_free($xml_parser);
+                    return false;
+                }
+                xml_parser_free($xml_parser);
+                return true;
+            }
         }
     }
-    
 }
